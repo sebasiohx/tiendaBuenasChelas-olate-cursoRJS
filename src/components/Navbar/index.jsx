ef3c7a1ff7eb993aced98818dd64dd4f3a8ee98d
Sebastian Olate H <seba.olate.h@gmail.com>
2022-01-17 15:36:42 -0300

    correccion atributo class a className

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,37 +1,38 @@
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Buenas Chelas</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Buenas Chelas</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto mr-lg-3">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto mr-lg-3">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Cervezas Nacionales</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Cervezas Nacionales</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Cervezas Importadas</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Cervezas Importadas</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
               Estilos
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Lager</a>
-              <a class="dropdown-item" href="#">Roja</a>
-              <a class="dropdown-item" href="#">Negra</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Lager</a>
+              <a className="dropdown-item" href="#">Roja</a>
+              <a className="dropdown-item" href="#">Negra</a>
             </div>
           </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="buscar marca" aria-label="Search"/>
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="search" placeholder="buscar marca" aria-label="Search"/>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
         </form>
+        <button className="btn btn-outline-secondary ml-2"><i className="fas fa-shopping-cart"></i><span className="cart-counter">99</span></button>
       </div>
     </nav>
   );
